4bb38a165bc2115555247c38f7f7f381df0d4e0c
Otabek Nabiyev <onabiyev626@gmail.com>
2025-01-13 12:35:55 +0300

    Delete TelegramBot directory

--- TelegramBot/src/main/java/org/example/telegrambot/entity/enums/Role.java
+++
@@ -1,6 +0,0 @@
-package org.example.telegrambot.entity.enums;
-
-public enum Role {
-    ADMIN,
-    USER
-}
--- TelegramBot/src/main/java/org/example/telegrambot/repository/ChannelRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.telegrambot.repository;
-
-import org.example.telegrambot.entity.Channel;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ChannelRepository extends CrudRepository<Channel, Long> {
-    Channel findByLink(String link);
-}
